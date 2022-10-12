b00d415e599e95854635b1749e769fc8808bf6bd
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2022-10-12 14:15:53 -0400

    Prepared for Capstone work to commence

---
+++ Assets/AnimStateController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimStateController : MonoBehaviour
+{
+    public Animation slabRaise;
+
+    //void OnDrag
+}
