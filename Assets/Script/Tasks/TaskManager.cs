c57da4834bddf8cee83d2d7ae1b7e4ce6580dc84
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2022-11-13 18:36:29 -0500

    Include URP VFX patch

--- Assets/Script/Tasks/TaskManager.cs
+++ Assets/Script/Tasks/TaskManager.cs
@@ -8,10 +8,10 @@ public class TaskManager : MonoBehaviour
 
     //an array of booleans to show whether task was completed successfully
     [SerializeField]
-    private bool[] taskComplete = new bool[3];
+    private bool[] taskCorrect = new bool[3];
 
     //an array of strings to select dialogue options
-    public string[] taskPrompt = new string[5];
+    public string[] taskPrompt = new string[5]; //MIGHT NOT NEED
 
     //array of our tasks in this playthrough
     public Task[] taskList = new Task[3];
@@ -20,7 +20,7 @@ public class TaskManager : MonoBehaviour
     public Task[] allTasks = new Task[5];
 
     //a random number between 0 and the length - 1 of the strings/tasks array
-    public int taskID;
+    public int taskID; //might not need
 
     //a boolean of whether or not the game has been won
     public bool winState;
@@ -33,6 +33,7 @@ public class TaskManager : MonoBehaviour
 
     //narrator's text
     public Text DoctorText;
+
 
     void Start()
     {
@@ -52,13 +53,11 @@ public class TaskManager : MonoBehaviour
 
         currentTaskIndex = 0;
         currentTask = taskList[currentTaskIndex];
-        currentTask.SetActive();
+        currentTask.SetActive(); //sets active variable to true;
         Invoke("OnActivation", 0.0f);
-
-        Task.TaskActivate += CurrentTaskListener;
     }
 
-    //
+    
     public void OnActivation()
     {
         TaskPrompt();
@@ -67,42 +66,56 @@ public class TaskManager : MonoBehaviour
     //Subscribed to the Completion event located in the task class
     public void OnCompletion()
     {
+        Debug.Log(currentTaskIndex + "ST TASK WAS COMPLETED");
+
+        //get the current task's correctness value and put it in the correctness array
+        taskCorrect[currentTaskIndex] = currentTask.GetCorrectStatus();
+
+        if(taskCorrect[currentTaskIndex] == false)
+        {
+            //play sad animation
+        }
+        else
+        {
+            //play happy animation
+        }
+
+        //increase the currentTask index
         currentTaskIndex += 1;
+
+        //set current task =to the next task in task List
         currentTask = taskList[currentTaskIndex];
+        currentTask.SetActive();
+        TaskPrompt();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        //if any other task gets compelted while its not active
+        //set current task correct to false
+        //move to next task.
     }
 
-    public void CurrentTaskListener()
-    {
-
-    }
-
-    
     public void TaskPrompt()
     {
+        //Have the doctor display the current task's text
         DoctorText.text = currentTask.GetTaskText();
-        Debug.Log(currentTask.GetTaskText());
     }
 
     public void TallyCorrects()
     {
         int scoreCount = 0;
 
-        for (int i = 0; i < taskComplete.Length; i++)
+        for (int i = 0; i < taskCorrect.Length; i++)
         {
-            if (taskComplete[i] == true)
+            if (taskCorrect[i] == true)
             {
                 scoreCount++;
             }
         }
 
         // after tallying score, check if we have won or lost
-        GetWinState(scoreCount, taskComplete);
+        GetWinState(scoreCount, taskCorrect);
     }
 
     public bool GetWinState(int points, bool[] taskArray)
---
+++ README.md
@@ -0,0 +1,21 @@
+# It's ALIVE!
+It's ALIVE! is a single-player VR experience in which you play the Doctor's Assistant. Your goal: to bring the doctor's creation to life!
+
+## About
+
+What does this do? You can go more here. Any inspiration? Info dump :D
+
+## Getting Started
+Hey! I want to run this on my PC, how do I do it? 
+
+In order to develop this game, you will need:
+
+Unity 2019.4 (.13 or higher, but check later)
+Universal Render Pipeline
+Shader Graph
+
+(i recommend doing one OS first, then if you want compatibility go back later later)
+### Pre-requisites (
+- do I need anything? Any one-click downloads ie unity? Any specific ver?
+### Instructions
+steps
